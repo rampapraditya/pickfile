a284f50bb972d12724291801d44af2d9f3a06eb4
rampapraditya <rampapraditya@gmail.com>
2024-11-19 10:25:03 +0700

    revisi

--- app/src/main/java/pramedia/pickfileandcapture/utils/FileUtils.java
+++ app/src/main/java/pramedia/pickfileandcapture/utils/FileUtils.java
@@ -3,13 +3,31 @@ package pramedia.pickfileandcapture.utils;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 public class FileUtils {
 
     public String getFilePathFromUri(Context context, Uri uri) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return getFileFromScopedStorage(context, uri);
+        } else {
+            return getFileFromLegacyStorage(context, uri);
+        }
+    }
+
+    private String getFileFromScopedStorage(Context context, Uri uri) {
+        // Gunakan pendekatan InputStream seperti pada contoh sebelumnya
+        return copyFileToCache(context, uri);
+    }
+
+    private String getFileFromLegacyStorage(Context context, Uri uri) {
         String path = null;
         if ("content".equals(uri.getScheme())) {
             String[] projection = {MediaStore.Images.Media.DATA};
@@ -25,6 +43,34 @@ public class FileUtils {
             path = uri.getPath();
         }
         return path;
+    }
+
+    private String copyFileToCache(Context context, Uri uri) {
+        String filePath = null;
+        String[] projection = {MediaStore.MediaColumns.DISPLAY_NAME};
+        try (Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                String fileName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME));
+                File cacheDir = context.getCacheDir();
+                File tempFile = new File(cacheDir, fileName);
+
+                try (InputStream inputStream = context.getContentResolver().openInputStream(uri);
+                     FileOutputStream outputStream = new FileOutputStream(tempFile)) {
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while (true) {
+                        assert inputStream != null;
+                        if (!((length = inputStream.read(buffer)) > 0)) break;
+                        outputStream.write(buffer, 0, length);
+                    }
+                }
+
+                filePath = tempFile.getAbsolutePath();
+            }
+        } catch (Exception e) {
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return filePath;
     }
 
     public String getFileNameFromUri(Context context, Uri uri) {
