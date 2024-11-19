8a1617579e9317f6a09ae5c0d4032f85b4d00609
rampapraditya <rampapraditya@gmail.com>
2024-11-19 11:06:00 +0700

    revisi

--- app/src/main/java/pramedia/pickfileandcapture/utils/ImageUtils.java
+++ app/src/main/java/pramedia/pickfileandcapture/utils/ImageUtils.java
@@ -3,7 +3,10 @@ package pramedia.pickfileandcapture.utils;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.widget.Toast;
 
 import java.io.InputStream;
@@ -13,10 +16,11 @@ public class ImageUtils {
     public Bitmap loadBitmapFromUri(Context context, Uri uri) {
         Bitmap bitmap = null;
         try {
-            InputStream inputStream = context.getContentResolver().openInputStream(uri);
-            bitmap = BitmapFactory.decodeStream(inputStream);
-            if (inputStream != null) {
-                inputStream.close();
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O_MR1) {
+                ImageDecoder.Source source = ImageDecoder.createSource(context.getContentResolver(), uri);
+                bitmap = ImageDecoder.decodeBitmap(source);
+            } else {
+                bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
             }
         } catch (Exception e) {
             Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
